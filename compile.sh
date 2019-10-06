92669a29edb8cf344cff9621d08054e4d437fd79
Kingdon Barrett <kingdon.b@nd.edu>
2019-10-06 11:38:14 -0400

    version 1.1

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 
 GOPATH=/Users/kbarret8/go
 docker run --rm -v "$GOPATH":/go -v "$PWD":/out golang:1.9-alpine3.7 go build -v -i -o /out/auto-lb src/github.com/kingdonb/docker-loadbalancer/main.go
-docker build . -t kingdonb/docker-loadbalancer:1.0-haproxy1.8-alpine3.7
+docker build . -t kingdonb/docker-loadbalancer:1.1-haproxy1.8-alpine3.7
